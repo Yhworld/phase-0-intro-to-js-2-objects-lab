008eed5185ebec5245b9d5e102f3f53b7abd0532
Yahya <yahyahussein670@gmail.com>
2022-06-02 13:37:14 +0300

    commit tree d47cc652040d1d9b60291a016afd432d413588ef

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name : 'yahya',
+    streetAddress : 1825
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key] : value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee['name'] = 'Sam'
+    employee['streetAddress'] = '12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee[key]}
+
+    delete newEmployee[key];
+
+    return newEmployee;
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+   }
+
+
